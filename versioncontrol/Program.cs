1a84d0a43948125afcc638e42c3a6c51cdf4f320
Truinto <47917181+Truinto@users.noreply.github.com>
2022-05-16 19:30:01 +0200

    update version control

--- versioncontrol/Program.cs
+++ versioncontrol/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace versioncontrol
 {
@@ -15,6 +16,8 @@ namespace versioncontrol
                     Console.WriteLine("not enough arguments");
                     return 2;
                 }
+
+                var rxVersion = new Regex("(Version.*\\\"|\\/v)([\\d\\.\\*]{5,})(\\\"|\\/)");
 
                 string path_changelog = args[0];
                 string path_assembly = args[1];
@@ -34,45 +37,56 @@ namespace versioncontrol
                     }
                 }
 
-
-                for (int i = 0; i < assembly.Length; i++)
+                if (version == null)
                 {
-                    if (assembly[i].StartsWith("[assembly: AssemblyVersion(\"", StringComparison.Ordinal))
-                    {
-                        assembly[i] = "[assembly: AssemblyVersion(\"" + version + "\")]";
-                    }
-                    else if (assembly[i].StartsWith("[assembly: AssemblyFileVersion(\"", StringComparison.Ordinal))
-                    {
-                        assembly[i] = "[assembly: AssemblyFileVersion(\"" + version + "\")]";
-                    }
+                    Console.WriteLine("Unable to parse version from changelog");
+                    return 0;
                 }
-                File.WriteAllLines(path_assembly, assembly);
 
-
-                for (int j = 2; j < args.Length; j++)
+                for (int i = 1; i < args.Length; i++)
                 {
-                    var info = File.ReadAllLines(args[j]);
-
-                    for (int i = 0; i < info.Length; i++)
-                    {
-                        if (info[i].Contains("\"Version\"", StringComparison.Ordinal))
-                        {
-                            int spaces; for (spaces = 0; spaces < info[i].Length && info[i][spaces] == ' '; spaces++) ;
-
-                            int index1 = info[i].LastIndexOf('"');
-                            if (index1 < 1) continue;
-
-                            int index2 = info[i].LastIndexOf('"', index1 - 1);
-
-                            if (index2 >= 0 && index2 < index1)
-                            {
-                                info[i] = info[i].Substring(0, index2 + 1) + version + info[i].Substring(index1);
-                            }
-                        }
-                    }
-
-                    File.WriteAllLines(args[j], info);
+                    string org = File.ReadAllText(args[i]);
+                    string repl = rxVersion.Replace(org, "${1}" + version + "${3}");
+                    File.WriteAllText(args[i], repl);
                 }
+
+                //for (int i = 0; i < assembly.Length; i++)
+                //{
+                //    if (assembly[i].StartsWith("[assembly: AssemblyVersion(\"", StringComparison.Ordinal))
+                //    {
+                //        assembly[i] = "[assembly: AssemblyVersion(\"" + version + "\")]";
+                //    }
+                //    else if (assembly[i].StartsWith("[assembly: AssemblyFileVersion(\"", StringComparison.Ordinal))
+                //    {
+                //        assembly[i] = "[assembly: AssemblyFileVersion(\"" + version + "\")]";
+                //    }
+                //}
+                //File.WriteAllLines(path_assembly, assembly);
+
+                //for (int j = 2; j < args.Length; j++)
+                //{
+                //    var info = File.ReadAllLines(args[j]);
+                //
+                //    for (int i = 0; i < info.Length; i++)
+                //    {
+                //        if (info[i].Contains("\"Version\"", StringComparison.Ordinal))
+                //        {
+                //            int spaces; for (spaces = 0; spaces < info[i].Length && info[i][spaces] == ' '; spaces++) ;
+                //
+                //            int index1 = info[i].LastIndexOf('"');
+                //            if (index1 < 1) continue;
+                //
+                //            int index2 = info[i].LastIndexOf('"', index1 - 1);
+                //
+                //            if (index2 >= 0 && index2 < index1)
+                //            {
+                //                info[i] = info[i].Substring(0, index2 + 1) + version + info[i].Substring(index1);
+                //            }
+                //        }
+                //    }
+                //
+                //    File.WriteAllLines(args[j], info);
+                //}
 
                 Console.WriteLine("Updated version numbers.");
                 return 0;
